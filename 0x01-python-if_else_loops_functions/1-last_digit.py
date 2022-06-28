fa217bfe30682e892082ebb1b4f1877ab6c9ad2f
Ahiamata-Gabriel <69096276+Ahiamata-Gabriel@users.noreply.github.com>
2022-06-28 15:06:50 +0000

    last digit correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-num_str = str(number)
-last_num_str = num_str[-1]
-last_num = int(last_num_str)
-str = "Last digit of {} is {}".format(number, last_num)
-if last_num > 5:
-    print("{} and is greater than 5".format(str))
-elif last_num == 0:
-    print("{} and is 0".format(str))
-elif last_num < 6 != 0:
-    print("{} and is less than 6 and not 0".format(str))
+str = "Last digit of"
+last_num = number % 10
+if (number < 0):
+    number *= -1
+    last_num = number % 10
+    last_num *= -1
+    number *= -1
+if (last_num > 5):
+    # print(f'{str} {number} is {last_num} and is greater than 5')
+    print("{} {} is {} and is greater than 5".format(str,number,last_num))
+elif (last_num == 0):
+    print("{} {} is {} and is 0".format(str, number, last_num))
+elif (last_num < 6 and last_num != 0):
+    print("{} {} is {} and is less than 6 and not 0".format(str, number, last_num))
