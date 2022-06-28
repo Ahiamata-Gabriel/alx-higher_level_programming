dc6ea701ca2779562c833075b4ca41a136074ae3
Ahiamata-Gabriel <69096276+Ahiamata-Gabriel@users.noreply.github.com>
2022-06-28 15:10:42 +0000

    last digit correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,8 +10,9 @@ if (number < 0):
     number *= -1
 if (last_num > 5):
     # print(f'{str} {number} is {last_num} and is greater than 5')
-    print("{} {} is {} and is greater than 5".format(str,number,last_num))
+    print("{} {} is {} and is greater than 5".format(str, number, last_num))
 elif (last_num == 0):
     print("{} {} is {} and is 0".format(str, number, last_num))
 elif (last_num < 6 and last_num != 0):
-    print("{} {} is {} and is less than 6 and not 0".format(str, number, last_num))
+    # print("{} {} is {} and is less than 6 and not 0".format(str, number, last_num))
+    print(f"{str} {number} is {last_num} and is less than 6 and not 0")
