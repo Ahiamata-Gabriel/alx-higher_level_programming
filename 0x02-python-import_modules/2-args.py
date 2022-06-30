a0b5407ee0f6cc9430a7284a7bbeddb969bfe2ea
Ahiamata-Gabriel <69096276+Ahiamata-Gabriel@users.noreply.github.com>
2022-06-30 20:55:46 +0000

    error correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
 
-import sys
-
-
-def main(*argv):
-    lstring = len(sys.argv) - 1
-    c = 0
-    if lstring > 1:
-        print("{:d} arguments:".format(lstring))
-        for i in sys.argv:
-            if c != 0:
-                print("{:d}: {}".format(c, i))
-            c += 1
-    elif lstring == 0:
-        print("{:d} arguments.".format(lstring))
-    else:
-        print("{:d} argument:".format(lstring))
-        print("{:d}: {}".format(1, sys.argv[1]))
-
-
 if __name__ == "__main__":
-    main()
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
