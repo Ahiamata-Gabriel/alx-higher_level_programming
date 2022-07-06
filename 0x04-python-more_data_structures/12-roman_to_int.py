76921c1265d478dd981d1f5a69cb5da0810a93de
Ahiamata-Gabriel <69096276+Ahiamata-Gabriel@users.noreply.github.com>
2022-07-06 16:53:33 +0000

    corrections

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,29 +2,30 @@
 
 
 def roman_to_int(roman_string):
+    """Converts a roman numeral to an integer."""
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
 
-    dict_r = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
     }
-    int_value = 0
+    num = 0
 
     for i in range(len(roman_string)):
-        if dict_r.get(roman_string[i], 0) == 0:
+        if roman_dict.get(roman_string[i], 0) == 0:
             return (0)
 
         if (i != (len(roman_string) - 1) and
-                dict_r[roman_string[i]] < dict_r[roman_string[i + 1]]):
-                int_value += dict_r[roman_string[i]] * -1
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+                num += roman_dict[roman_string[i]] * -1
 
         else:
-            int_value += dict_r[roman_string[i]]
-    return (int_value)
+            num += roman_dict[roman_string[i]]
+    return (num)
